41f3ca1eafc641e3cd38bc27a7f2d4414c7cc075
RusFeniks <kd.feniks@gmail.com>
2023-11-09 16:10:21 +0300

    Сделал компоненты футера вложенными

--- src/component/footer/footer.component.tsx
+++ src/component/footer/footer.component.tsx
@@ -1,9 +1,9 @@
 import classNames from "classnames";
-import Authorization from "../authorization/authorization.component";
-import GameStartButton from "../game-start-button/game-start-button.component";
-import UpdateButton from "../update-button/update-button.component";
-import UpdateProgressBar from "../update-progress-bar/update-progress-bar.component";
+import Authorization from "./authorization/authorization.component";
 import style from "./footer.styles.module.scss";
+import GameStartButton from "./game-start-button/game-start-button.component";
+import UpdateButton from "./update-button/update-button.component";
+import UpdateProgressBar from "./update-progress-bar/update-progress-bar.component";
 
 interface FooterProps {
   className?: string;
