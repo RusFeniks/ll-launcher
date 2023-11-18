6819c277ccf28d685a9dd912084fba5c8a33c170
RusFeniks <kd.feniks@gmail.com>
2023-11-18 14:54:36 +0300

    Добавил API_URL в env переменные для клиентских компонентов, выключил strictMode

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
     images: {
         unoptimized: true
     },
+    reactStrictMode: false,
+    env: {
+        API_URL: process.env.API_URL
+    },
 }
 
 module.exports = nextConfig
